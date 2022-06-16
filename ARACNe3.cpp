197aa6a298820cec117903ff973ace5fbf3ab662
arhowe00 <arhowe00@gmail.com>
2022-06-16 14:14:04 -0400

    Modified main and ARACNe3 function for subnets

--- ARACNe3.cpp
+++ ARACNe3.cpp
@@ -9,6 +9,7 @@ uint32_t size_of_network = 0;
 uint16_t tot_num_samps = 0;
 uint16_t tot_num_regulators = 0;
 
+
 /*
  These variables are tuned according to user preferences.  Some of these the user doesn't choose, such as the cached_dir, which is always the working directory of the ARACNe3 script.
  */
@@ -18,8 +19,11 @@ double subsampling_percent = 1 - std::exp(-1);
 bool prune_MaxEnt = true;
 bool verbose = true;
 std::string cached_dir;
+std::string output_dir;
+std::string log_dir;
 uint32_t global_seed = 0;
 float DEVELOPER_mi_cutoff = 0;
+uint16_t num_subnets = 1;
 
 extern uint32_t num_null_marginals;
 
@@ -27,48 +31,26 @@ extern uint32_t num_null_marginals;
  Convenient function for timing parts of ARACNe3.  It's only used to time from the pipeline function, so it's included in ARACNe3.cpp.
  */
 static auto last = std::chrono::high_resolution_clock::now(), cur = std::chrono::high_resolution_clock::now();
-static void sinceLast() {
+static void sinceLast(std::ostream &ostream) {
 	cur = std::chrono::high_resolution_clock::now();
-	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(cur-last).count() << "ms" << std::endl;
+	ostream << std::chrono::duration_cast<std::chrono::milliseconds>(cur-last).count() << "ms" << std::endl;
 	last = cur;
 }
 
 /*
  This function is the ARACNe3 main pipeline, called from main().  The main function just parses command line arguments and options, and it sets global variables, before calling the ARACNe3 function here.
  */
-void ARACNe3(string normalized_exp_mat_tsv_filename = "exp_mat.txt", string newline_separated_regulator_list_file = "regulators.txt", string output_dir = "output", double subsampling_percent = (1 - std::exp(-1)), bool prune_FDR = true, float FDR = 0.05f, bool prune_MaxEnt = true, bool verbose = true) {
-	if (verbose) {
-		// we don't print "... TIME:" here because we have prints in the function below
-		//-------time module-------
-		last = chrono::high_resolution_clock::now();
-		//-------------------------
-	}
+void ARACNe3(genemap *matrix_ptr, uint16_t subnet_idx) {
+	// set the individual subnet log file
+	std::ofstream log_output(log_dir + "log_subnet" + std::to_string(subnet_idx) + ".txt");
 	
-	readRegList(newline_separated_regulator_list_file);
-	genemap matrix = readExpMatrix(normalized_exp_mat_tsv_filename, subsampling_percent);
+	genemap matrix = *matrix_ptr;
+	
 	
 	if (verbose) {
 		//-------time module-------
-		std::cout << "\nMATRIX & REGULATORS READ TIME:" << std::endl;
-		sinceLast();
-		//-------------------------
-		
-		//-------time module-------
-		cout << "\nNULL MI MODEL TIME:" << endl;
-		last = chrono::high_resolution_clock::now();
-		//-------------------------
-	}
-	
-	initNullMIs(tot_num_samps);
-	
-	if (verbose) {
-		//-------time module-------
-		sinceLast();
-		//-------------------------
-	
-		//-------time module-------
-		cout << "\nRAW NETWORK COMPUTATION TIME:" << endl;
-		last = chrono::high_resolution_clock::now();
+		log_output << "\nRAW NETWORK COMPUTATION TIME:" << std::endl;
+		last = std::chrono::high_resolution_clock::now();
 		//-------------------------
 	}
 	
@@ -81,8 +63,8 @@ void ARACNe3(string normalized_exp_mat_tsv_filename = "exp_mat.txt", string newl
 	
 	if (verbose) {
 		//-------time module-------
-		sinceLast();
-		cout << "SIZE OF NETWORK: " << size_of_network << " EDGES." << endl;
+		sinceLast(log_output);
+		log_output << "SIZE OF NETWORK: " << size_of_network << " EDGES." << std::endl;
 		//-------------------------
 	}
 	
@@ -90,8 +72,8 @@ void ARACNe3(string normalized_exp_mat_tsv_filename = "exp_mat.txt", string newl
 		if (!prune_FDR) FDR = 1.01f; // we must set to 1.01f to preserve all edges; rounding issue.
 		if (verbose) {
 			//-------time module-------
-			cout << "\nFDR PRUNING TIME:" << endl;
-			last = chrono::high_resolution_clock::now();
+			log_output << "\nFDR PRUNING TIME:" << std::endl;
+			last = std::chrono::high_resolution_clock::now();
 			//-------------------------
 		}
 		
@@ -102,8 +84,8 @@ void ARACNe3(string normalized_exp_mat_tsv_filename = "exp_mat.txt", string newl
 		
 		if (verbose) {
 			//-------time module-------
-			sinceLast();
-			cout << "SIZE OF NETWORK: " << size_of_network << " EDGES." << endl;
+			sinceLast(log_output);
+			log_output << "SIZE OF NETWORK: " << size_of_network << " EDGES." << std::endl;
 			//-------------------------
 		}
 		
@@ -111,7 +93,7 @@ void ARACNe3(string normalized_exp_mat_tsv_filename = "exp_mat.txt", string newl
 			// you _must_ prune FDR to do MaxEnt, but you can always FDR = 1.00
 			if (verbose) {
 				//-------time module-------
-				std::cout << "\nMaxEnt PRUNING TIME:" << std::endl;
+				log_output << "\nMaxEnt PRUNING TIME:" << std::endl;
 				last = std::chrono::high_resolution_clock::now();
 				//-------------------------
 			}
@@ -121,8 +103,8 @@ void ARACNe3(string normalized_exp_mat_tsv_filename = "exp_mat.txt", string newl
 			
 			if (verbose) {
 				//-------time module-------
-				sinceLast();
-				std::cout << "SIZE OF NETWORK: " << size_of_network << " EDGES." << std::endl;
+				sinceLast(log_output);
+				log_output << "SIZE OF NETWORK: " << size_of_network << " EDGES." << std::endl;
 				//-------------------------
 			}
 		}
@@ -130,36 +112,18 @@ void ARACNe3(string normalized_exp_mat_tsv_filename = "exp_mat.txt", string newl
 	
 	if (verbose) {
 		//-------time module-------
-		std::cout << "\nPRINTING NETWORK IN DIRECTORY \"" + output_dir + "\"....." << std::endl;
+		log_output << "\nPRINTING NETWORK IN DIRECTORY \"" + output_dir + "\"....." << std::endl;
 		last = chrono::high_resolution_clock::now();
 		//-------------------------
 	}
 	
-	writeNetworkRegTarMI(network, output_dir, std::to_string(size_of_network));
+	// writes the individual subnet output
+	writeNetworkRegTarMI(network, output_dir, "subnet" + std::to_string(subnet_idx));
 	
 	if (verbose) {
 		//-------time module-------
-		sinceLast();
+		sinceLast(log_output);
 		//-------------------------
-		using namespace std::string_literals;
-		const char* success_A3 =
-R"(
-
-                |
-                |
-                ;                            Aaron T. Griffin
-                ;                            Lukas J. Vlahos
-                |                            Andrew R. Howe
-           ,  / | \   ,
-         , ;_/ ,L-, `_;  ,
-         \._/.ARACNe3.\_./
-           \_./(::)\._/                      Andrea Califano
-                ''
-
-
-Success!
-)";
-		std::cout << success_A3 << std::endl;
 	}
 }
 
@@ -199,9 +163,10 @@ int main(int argc, char *argv[]) {
 	
 	//--------------------cmd line parsing------------------------
 	
-	string matrix = (string) getCmdOption(argv, argv+argc, "-e");
-	string regulators = (string) getCmdOption(argv, argv+argc, "-r");
-	string output_dir = (string) getCmdOption(argv, argv+argc, "-o");
+	string exp_file = (string) getCmdOption(argv, argv+argc, "-e");
+	string reg_file = (string) getCmdOption(argv, argv+argc, "-r");
+	
+	output_dir = (string) getCmdOption(argv, argv+argc, "-o");
 	
 	// make sure output_dir has a trailing slash
 	if (output_dir.back() != '/')
@@ -224,6 +189,12 @@ int main(int argc, char *argv[]) {
 			std::cout << "Subsampling percent not on range (0,1]; setting to 1.00." << std::endl;
 			subsampling_percent = 1.00;
 	}
+	
+	if (cmdOptionExists(argv, argv+argc, "-x"))
+		num_subnets = stoi(getCmdOption(argv, argv+argc, "-x"));
+
+	if (cmdOptionExists(argv, argv+argc, "--numNetworks"))
+		num_subnets = stoi(getCmdOption(argv, argv+argc, "--numNetworks"));
 
 	if (cmdOptionExists(argv, argv+argc, "--noFDR"))
 	    prune_FDR = false;
@@ -231,6 +202,7 @@ int main(int argc, char *argv[]) {
 	    prune_MaxEnt = false;
 	if (cmdOptionExists(argv, argv+argc, "--noverbose"))
 	    verbose = false;
+
 	//----------------------DEVELOPER--------------------------
 	
 	if (cmdOptionExists(argv, argv+argc, "--mithresh"))
@@ -252,9 +224,71 @@ int main(int argc, char *argv[]) {
 
 	cached_dir = "./"+ hiddenfpre + "ARACNe3_cached/";
 	
-	makeOutputDir(output_dir);
-	makeCachedDir(cached_dir);
+	makeDir(output_dir);
+	makeDir(cached_dir);
 	
-	ARACNe3(matrix, regulators, output_dir, subsampling_percent, prune_FDR, FDR, prune_MaxEnt, verbose);
+	log_dir = output_dir + "log/";
+	makeDir(log_dir);
+	
+	if (verbose) {
+	// we don't print "... TIME:" here because we have prints in the function below
+		//-------time module-------
+		last = chrono::high_resolution_clock::now();
+		//-------------------------
+	}
+	
+	readRegList(reg_file);
+	std::vector<genemap> matrices = readExpMatrix(exp_file);
+	
+	if (verbose) {
+		//-------time module-------
+		std::cout << "\nMATRIX & REGULATORS READ TIME:" << std::endl;
+		sinceLast(std::cout);
+		//-------------------------
+		
+		//-------time module-------
+		std::cout << "\nNULL MI MODEL TIME:" << endl;
+		last = chrono::high_resolution_clock::now();
+		//-------------------------
+	}
+	
+	initNullMIs(tot_num_samps);
+	
+	if(verbose) {
+		//-------time module-------
+		sinceLast(std::cout);
+		//-------------------------
+		
+		//-------time module-------
+		std::cout << "\nCREATING NETWORK(s)..." << std::endl;
+		//-------------------------
+	}
+	
+	for (uint16_t i = 0; i < num_subnets; ++i) {
+		ARACNe3(&matrices[i], i);
+	}
+	
+	if (verbose) {
+		using namespace std::string_literals;
+		const char* success_A3 =
+R"(
+
+                |
+                |
+                ;                            Aaron T. Griffin
+                ;                            Lukas J. Vlahos
+                |                            Andrew R. Howe
+           ,  / | \   ,
+         , ;_/ ,L-, `_;  ,
+         \._/.ARACNe3.\_./
+           \_./(::)\._/                      Andrea Califano
+                ''
+
+
+Success!
+)";
+		std::cout << success_A3 << std::endl;
+	}
+	
 	return 0;
 }
