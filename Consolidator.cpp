e9dc3a4827669f9b75a2f4d36f4661e3ded754d6
arhowe00 <arhowe00@gmail.com>
2022-06-23 14:42:30 -0400

    Revised theta for right-tailed p Prepared to use FPR estimate by making global variable

--- Consolidator.cpp
+++ Consolidator.cpp
@@ -4,11 +4,13 @@
 
 #include "ARACNe3.hpp"
 
+float FPR_estimate = 1.5E-4f;
+
 extern uint16_t tot_num_samps_pre_subsample;
 extern uint16_t tot_num_samps;
 extern uint16_t tot_num_regulators;
 extern genemap global_gm;
-extern genemap_r global_gm_r;
+extern genemap_r global_gm_r; 
 extern uint16_t num_subnets;
 
 float consolidate_scc(const std::vector<uint16_t>& x_ranked, const std::vector<uint16_t>& y_ranked) {
@@ -24,12 +26,11 @@ double lchoose(const uint16_t &n, const uint16_t &k) {
 }
 
 double right_tail_binomial_p(const uint16_t& num_occurrences) {
-	float theta = 1.5E-4f;
 	double p = 0.0;
 	if (num_subnets == 1)
 		return std::numeric_limits<double>::quiet_NaN(); // cannot have a p-value for 1 subnet (1 network)
 	for (uint16_t i = num_subnets; i >= num_occurrences; --i)
-		p += std::exp(lchoose(num_subnets, num_occurrences) + num_occurrences * std::log(theta) + (num_subnets - num_occurrences) * std::log(1-theta));
+		p += std::exp(lchoose(num_subnets, num_occurrences) + num_occurrences * std::log(FPR_estimate) + (num_subnets - num_occurrences) * std::log(1-FPR_estimate));
 	return p;
 }
 
