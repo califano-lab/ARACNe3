57a26097120d8e943090200927cc42bbe31a5b8d
arhowe00 <arhowe00@gmail.com>
2022-07-08 18:01:58 -0400

    Updated out-of-order subnet generation warning

--- ARACNe3.cpp
+++ ARACNe3.cpp
@@ -340,7 +340,7 @@ int main(int argc, char *argv[]) {
 		}
 		num_subnets = i;
 	} else {
-		if (nthreads > 1)
+		if (nthreads > 1 && num_subnets > 1)
 			std::cout << "Note: Because more than one thread is used to compute a fixed number of subnetworks, their completion times may not be in order.  This is not an error." << std::endl;
 		subnets = std::vector<reg_web>(num_subnets);
 #pragma omp parallel for num_threads(nthreads)
