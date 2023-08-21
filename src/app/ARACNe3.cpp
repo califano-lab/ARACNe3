1a1fdda92b47980946cd0581658add3edbed343c
arhowe00 <arhowe00@gmail.com>
2023-08-21 14:45:18 -0400

    ARACNe3.cpp: Improve commenting

--- src/app/ARACNe3.cpp
+++ src/app/ARACNe3.cpp
@@ -140,6 +140,8 @@ int main(int argc, char *argv[]) {
   }
 
   //--------------------------------------------------------------
+  //                       Begin ARACNe3                       
+  //--------------------------------------------------------------
 
   const std::string subnets_dir =
       makeUnixDirectoryNameUniversal(output_dir + "subnets_" + runid + "/");
@@ -265,8 +267,8 @@ int main(int argc, char *argv[]) {
       subnets = std::vector<gene_to_gene_to_float>(num_subnets);
       FPR_estimates = std::vector<float>(num_subnets);
       for (int i = 0; i < num_subnets; ++i) {
-        gene_to_floats subsample_exp_mat = sampleExpMatAndReCopulaTransform(
-              exp_mat, tot_num_subsample, rand);
+        gene_to_floats subsample_exp_mat =
+            sampleExpMatAndReCopulaTransform(exp_mat, tot_num_subsample, rand);
         const auto &[subnet, FPR_estimate_subnet] = createARACNe3Subnet(
             subsample_exp_mat, regulators, genes, tot_num_samps,
             tot_num_subsample, i, prune_alpha, nullmodel, method, alpha,
