57cafe47c6fe925531128d9869fad212fc708e46
arhowe00 <arhowe00@gmail.com>
2024-04-08 18:15:44 -0400

    main: fully abstract analysis from main

--- include/ARACNe3/analysis.hpp
+++ include/ARACNe3/analysis.hpp
@@ -5,14 +5,14 @@
 #include "aracne3io.hpp"
 #include "logger.hpp"
 
-int ARACNe3Analysis(const ARACNe3IOHandler &io, const std::string &version,
-             const std::string &runid, const uint32_t seed,
-             const uint8_t threads, const bool verbose, const float alpha,
-             const float subsamp_pct, const uint32_t n_nulls,
-             const std::string &method, const bool prune_alpha,
-             const bool prune_MaxEnt, const bool save_subnets,
-             const std::string &subnets_log_dir, const bool adaptive,
-             const uint16_t min_subnets, const uint16_t max_subnets,
-             const uint16_t min_regulon_occpuancy, const bool consolidate_mode,
-             const bool skip_consolidate, Logger *const aracne3_logger,
-             const std::string &subnets_dir, const std::string &cached_dir);
+int ARACNe3Analysis(
+    const ARACNe3IOHandler &io, const std::string &version,
+    const std::string &runid, const uint32_t seed, const uint8_t threads,
+    const bool verbose, const float alpha, const float subsamp_pct,
+    const uint32_t n_nulls, const std::string &method, const bool prune_alpha,
+    const bool prune_MaxEnt, const bool save_subnets,
+    const std::string &subnets_log_dir, uint16_t n_subnets,
+    const bool adaptive, const uint16_t min_subnets, const uint16_t max_subnets,
+    const uint16_t min_regulon_occpuancy, const bool consolidate_mode,
+    const bool skip_consolidate, Logger *const aracne3_logger,
+    const std::string &subnets_dir, const std::string &cached_dir);
