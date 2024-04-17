c028f39d5918ed152e7a1aceee8e80dd07a61908
arhowe00 <arhowe00@gmail.com>
2024-04-16 23:09:20 -0400

    fsio: Mv exception definitions to hpp

--- include/ARACNe3/fsio.hpp
+++ include/ARACNe3/fsio.hpp
@@ -36,3 +36,44 @@ private:
       final_output_file_name, subnets_dir, runid;
   const char sep;
 };
+
+class FileNotOpenException : public std::runtime_error {
+public:
+  explicit FileNotOpenException(const std::string &filename)
+      : std::runtime_error("Could not open file: " + filename) {}
+};
+
+class DirectoryReadException : public std::runtime_error {
+public:
+  explicit DirectoryReadException(const std::string &directoryPath)
+      : std::runtime_error("Error reading directory: " + directoryPath) {}
+};
+
+class NoRegulatorsException : public std::runtime_error {
+public:
+  explicit NoRegulatorsException(const std::string &filename)
+      : std::runtime_error("No regulators found in file: " + filename) {}
+};
+
+class MatrixDataException : public std::runtime_error {
+public:
+  explicit MatrixDataException(const std::string &message)
+      : std::runtime_error(message) {}
+};
+
+class FileNotFoundException : public std::runtime_error {
+public:
+  explicit FileNotFoundException(const std::string &filename,
+                                 const std::string &msg = "")
+      : std::runtime_error("File not found: " + filename + ". " + msg) {}
+};
+
+class GeneInSubnetNotInMatrixException : public std::runtime_error {
+public:
+  explicit GeneInSubnetNotInMatrixException(const std::string &gene)
+      : std::runtime_error(
+            "Subnetwork file cannot contain genes gene names undefined in the "
+            "expression matrix. You should only consolidate subnetworks from "
+            "the expression matrix used to generate them. (" +
+            gene + ")") {}
+};
